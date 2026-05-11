0a335c47e9694262983a58adef909fff23fbb539
NGUYỄN DOÃN VY LINH <nguyendoanvylinh@Vy-Linh.local>
2026-05-11 16:42:51 +0700

    fix: sửa lỗi chính tả tên Blog thành Vy Linh's Blog

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Vỹ Linh's Blog";
-export const SITE_DESCRIPTION = "Chào mừng bạn đến với blog cá nhân của Nguyễn Đoàn Vỹ Linh!";
+export const SITE_TITLE = "Vy Linh's Blog";
+export const SITE_DESCRIPTION = "Chào mừng bạn đến với blog cá nhân của Nguyễn Doãn Vy Linh!";
